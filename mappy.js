31024459616ebb70c20699b29947a41df36dc10c
Kiana Tavakoli <kianatavakolii@gmail.com>
2019-07-12 14:47:52 -0700

    Update map

--- mappy.js
+++ mappy.js
@@ -2,7 +2,11 @@ var view;
 var map;
 var initiallocation;
 var london;
-var panHome;
+// var panHome;
+var zoom;
+var parts = 2;
+var called = false;
+
 
 function init() {
   initiallocation = ol.proj.fromLonLat([41.043316, 28.862457]);
@@ -29,12 +33,43 @@ function init() {
   });
 }
 
-function panHome() {
+// function panHome() {
+//   view.animate({
+//     center: london,
+//     duration: 2000
+//   });
+// }
+function flyToLondon() {
+  var duration = 2000;
+  var zoom = view.getZoom();
+  var parts = 2;
+  var called = false;
+  // function callback(complete) {
+  //   --parts;
+  //   if (called) {
+  //     return;
+  //   }
+  //   if (parts === 0 || !complete) {
+  //     called = true;
+  //     done(complete);
+  //   }
+  // }
   view.animate({
-    center: london,
-    duration: 2000
+    center: location,
+    duration: duration
+  });
+  view.animate({
+    zoom: zoom - 1,
+    duration: duration / 2
+  }, {
+    zoom: zoom,
+    duration: duration / 2
   });
 }
+
+// onClick('flyToLondon', function() {
+//   flyTo(london, function() {});
+// });
 
 function goToPhilz() {
   view.animate({
